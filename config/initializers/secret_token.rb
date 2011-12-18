b42aeec4dbaf472def8f96d4c43b01938f98edab
Brian Jordan <bcjordan@gmail.com>
2011-12-17 22:58:52 -0500

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newgifurl::Application.config.secret_token = '6a4b0ce1560bd174fe7979115b1c793074ccdf68d8561633883a9d0ec59b7598f8d0b5b811dff5fb5b5c96985a6b625435d904f565c36696113f15b95d9d62c6'
